a91ef716e219329f14d6f3bd57fb6a3fe56d2d9c
Mabende <belindamabende@gmail.com>
2024-06-17 03:35:20 +0200

    Initial Commit

---
+++ selfiegram/selfiegramApp.swift
@@ -0,0 +1,17 @@
+//
+//  selfiegramApp.swift
+//  selfiegram
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/06/17.
+//
+
+import SwiftUI
+
+@main
+struct selfiegramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
